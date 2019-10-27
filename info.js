c019794765352ce906aceaa6ebe015e754d254c4
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2019-10-26 20:23:17 -0700

    One more commit

--- info.js
+++ info.js
@@ -452,3 +452,34 @@ const promises = [checkSunglasses, checkPants, checkBags]
 Promise.all(promises)
   .then(onFulfill)
   .catch(onReject)
+
+//Async await! finally!
+//Refactoring promises:
+function withConstructor(num){
+  return new Promise((resolve, reject) => {
+    if (num === 0){
+      resolve('zero');
+    } else {
+      resolve('not zero');
+    }
+  })
+}
+
+withConstructor(0)
+  .then((resolveValue) => {
+  console.log(` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`);
+})
+
+//Refactors to:
+async function withAsync(num){
+  if (num === 0){
+      return 'zero';
+    } else {
+      return 'not zero';
+    }
+}
+
+withAsync(100)
+  .then((resolveValue) => {
+  console.log(` withAsync(100) returned a promise which resolved to: ${resolveValue}.`);
+})
