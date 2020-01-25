cda6476287e43e56a3f24ff3978425660dd95b6c
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2020-01-24 22:59:34 -0800

    CODEWARS!

--- codewars/kata3.js
+++ codewars/kata3.js
@@ -1,6 +1,15 @@
 //kata 3
 //https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
 descendingOrder = n => {
-  let a = n.split("")
-
+  if ((n.length = 1)) {
+    return n;
+  } else {
+    let n2 = n.toString();
+    let n3 = n2.split("");
+    //parseInt?
+    let n4 = n3.map(x => parseInt(x,10))
+    n4.sort((a, b) => {
+      return b - a;
+    });
+  }
 };
