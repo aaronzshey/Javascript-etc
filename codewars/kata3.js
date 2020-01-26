0cc18a7b4ce6eda2fcb85f95f23589ddc010d741
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2020-01-25 22:58:27 -0800

    Finally finished that codewars kata!  Took two days and I am so damn happy.

--- codewars/kata3.js
+++ codewars/kata3.js
@@ -1,15 +1,18 @@
 //kata 3
 //https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
-descendingOrder = n => {
-  if ((n.length = 1)) {
+const descendingOrder = n => {
+  if (n.length === 1) {
     return n;
   } else {
-    let n2 = n.toString();
-    let n3 = n2.split("");
-    //parseInt?
-    let n4 = n3.map(x => parseInt(x,10))
-    n4.sort((a, b) => {
-      return b - a;
-    });
+    let n2 = [...n.toString(10)];
+    let n3 = n2.map(x => parseInt(x, 10));
+    //the ellpsis syntax converts each element of the array into an argument
+    //preventing us from having to split each elem apart and do JS magic with it
+    n3.sort((a, b) => b - a);
+    let n4 = n3.join("");
+    let n5 = parseInt(n4, 10);
+    return n5;
   }
 };
+descendingOrder(123456789);
+a;
