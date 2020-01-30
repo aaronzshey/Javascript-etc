1f09b3e080b30136943d6703783661e91fb1472f
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2020-01-29 23:01:30 -0800

    Codewars

--- codewars/kata6.js
+++ codewars/kata6.js
@@ -1,14 +1,16 @@
 //https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/javascript
 const nbYear = (p0, percent, aug, p) => {
   // aug (inhabitants coming or leaving each year), p (population to surpass)
-  let multiplier = asdf;
-  let newPopulation1 = p0;
-  let newPopulation2 = newPopulation1 + aug;
-  let convertedPercent = percent / 100;
-  //define population???
-  let a = Math.log10(population / p0);
-  let b = Math.log10(convertedPercent);
-  let yr = a / b;
 
-  do {} while (true);
+  let convertedPercent = 1 + percent / 100;
+  //define population???
+
+
+
+  while (finalPopulation < yr) {
+    var a = Math.pow(convertedPercent,yr)
+    var finalPopulation = a + aug
+    yr++;
+    if (finalPopulation < yr){ return yr }
+  }
 };
