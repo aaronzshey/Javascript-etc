b7434aaa6be65d265fbd70222b6dfe02102d0618
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2020-02-16 23:21:27 -0800

    Reconfiguring webscrape file for Apple Music, adding to website version soon

--- webscrape.js
+++ webscrape.js
@@ -1,13 +1,15 @@
 //https://buttercms.com/blog/web-scraping-with-nodejs-and-cheerio
 const cheerio = require("cheerio");
 const axios = require("axios");
-let a = "https://missfiatlux.tumblr.com/post/189790973620";
+let a = "https://music.apple.com/us/album/all-the-way-over/1492516293";
 axios
   .get(a)
   .then(response => {
     const $ = cheerio.load(response.data);
-    const img = $("img");
-    console.log(img['0'].attribs.src);
+    //const songTitle = $("span.product-header__title");
+    //console.log(songTitle[0].children[0].data)
+    const releaseDate = $("span.link-list__item__date");
+    console.log(releaseDate[0].children[0].data)
   })
   .catch(function(error) {
     console.log(error);
