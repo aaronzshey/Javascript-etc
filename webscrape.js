09cbc5eb4f42ccdc85107d33a9e65b354ea5cd32
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2020-02-18 22:37:38 -0800

    Refined output messages.

--- webscrape.js
+++ webscrape.js
@@ -1,15 +1,17 @@
 //https://buttercms.com/blog/web-scraping-with-nodejs-and-cheerio
 const cheerio = require("cheerio");
 const axios = require("axios");
-let a = "https://music.apple.com/us/album/all-the-way-over/1492516293";
+let a = "https://music.apple.com/us/album/mess-without-u-single/1488410124";
 axios
   .get(a)
   .then(response => {
     const $ = cheerio.load(response.data);
-    const songTitle = $("span.product-header__title");
-    console.log(songTitle[0].children[0].data)
-    const releaseDate = $("span.link-list__item__date");
-    console.log(releaseDate[0].children[0].data)
+    const songTitleInput = $("span.product-header__title");
+    const songTitle = songTitleInput[0].children[0].data
+    console.log(`Song Title: ${songTitle}`)
+    const releaseDateInput = $("span.link-list__item__date");
+    const releaseDate = releaseDateInput[0].children[0].data
+    console.log(`Release Date: ${releaseDate}`)
 
   })
   .catch(function(error) {
