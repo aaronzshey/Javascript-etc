b2e76d0b14b31453fe812dbfab4627aef31570d8
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2020-02-18 16:23:32 -0800

    Removed comments

--- webscrape.js
+++ webscrape.js
@@ -6,11 +6,11 @@ axios
   .get(a)
   .then(response => {
     const $ = cheerio.load(response.data);
-    //const songTitle = $("span.product-header__title");
-    //console.log(songTitle[0].children[0].data)
+    const songTitle = $("span.product-header__title");
+    console.log(songTitle[0].children[0].data)
     const releaseDate = $("span.link-list__item__date");
     console.log(releaseDate[0].children[0].data)
-    
+
   })
   .catch(function(error) {
     console.log(error);
